22655e1557e69f9b0343bdb820c7788089ce8090
u_suke <u_suke.a@icloud.com>
2019-10-13 16:48:50 +0900

    中分類クリックで小分類ごとの表示実装

--- grandmenu/static/js/script.js
+++ grandmenu/static/js/script.js
@@ -1,4 +1,4 @@
-// Lightboxの追加関数
+// Lightboxを表示する
 $(function() {
 	$(".button__add").click(function(){
 		$(".lightbox").css(
@@ -7,6 +7,39 @@ $(function() {
 	});
 });
 
+// Lightboxを隠す
+$(function() {
+  $(".lightbox").dblclick(function(){
+    $(".lightbox").css(
+        "display", "none"
+      );
+  });
+});
+
+// 小分類メニュー表示
+$(function() {
+  $(".menu-box").click(function(){
+    var selector = $(this).attr('value')
+      $(".lightbox__menu-box").css(
+        "display", "flex"
+      );
+      $("." + selector).css(
+          "display", "inline-block"
+        );
+      });
+    });
+
+// 小分類のメニュー消す
+$(function() {
+  $(".lightbox__menu-box").dblclick(function(){
+    $(".lightbox__menu-box li").css(
+        "display", "none"
+      );
+    $(".lightbox__menu-box").css(
+        "display", "none"
+      );
+  });
+});
 
 // ドラック&ドロップでメニューの順番を並べ替える関数
 $(function() {
