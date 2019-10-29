2a3ec64adb83560d4cf3d00a9ba39a2067ac0adf
u_suke <u_suke.a@icloud.com>
2019-10-30 00:37:28 +0900

    ユーザー権限の追加等、データベース一新

--- grandmenu/app.py
+++ grandmenu/app.py
@@ -50,58 +50,119 @@ def verify_password(hash_pass, original_pass):
     return check_password_hash(hash_pass, original_pass)
 # 関数エリア-End
 
-#Userテーブル定義
+#テーブル定義
 class Store(db.Model):
     __tablename__ = 'stores'
 
-    STORE_ID = db.Column(Integer, primary_key=True)
+    STORE_ID = db.Column(Integer, primary_key=True) # ≒代表者のSTAFF_ID
     STORE_NAME = db.Column(String(255))
+
+    def __repr__(self):
+        return "(STORE_ID='%s', STORE_NAME='%s')" % (self.STORE_ID, self.STORE_NAME)
+
+class Staff(db.Model):
+    __tablename__ = 'staffs'
+
+    STAFF_ID = db.Column(Integer, primary_key=True)
+    STORE_ID = db.Column(Integer)
+    STAFF_NUMBER = db.Column(Integer)   #スタッフ順の並べ替えの際に必要
+    STAFF_NAME = db.Column(String(255))
     E_MAIL = db.Column(String(255), unique=True)
     PASSWORD = db.Column(String(255))
+    STAFF_CLASS_ID = db.Column(Integer)
+    STAFF_CLASS = db.Column(String(32))
 
     def __repr__(self):
-        return "(STORE_ID='%s', STORE_NAME='%s', E_MAIL='%s', PASSWORD='%s')" % (self.STORE_ID, self.STORE_NAME, self.E_MAIL, self.PASSWORD)
+        return "(STAFF_ID='%s', STORE_ID='%s', STAFF_NUMBER='%s', STAFF_NAME='%s', , E_MAIL='%s', PASSWORD='%s', STAFF_CLASS_ID='%s',STAFF_CLASS='%s')" % (self.STAFF_ID, self.STORE_ID, self.STAFF_NUMBER, self.STAFF_NAME, self.E_MAIL, self.PASSWORD, self.STAFF_CLASS_ID, self.STAFF_CLASS)
 
-#Food_Drinkテーブル定義
-class Food_Drink(db.Model):
-    __tablename__ = 'food_drink'
+#Class_Middleテーブル定義
+class Menu(db.Model):
+    __tablename__ = 'menus'
 
-    ID = db.Column(Integer, primary_key=True)
-    USER_ID = db.Column(Integer)#20190430
-    KIND_ID = db.Column(Integer)
-    KIND = db.Column(String(5))
-    CLASS_MIDDLE_ID = db.Column(Integer)
-    CLASS_MIDDLE = db.Column(String(64))
-    NAME_OF_DISH_ID = db.Column(Integer)
-    NAME_OF_DISH = db.Column(String(64), unique=True)
-    PRICE = db.Column(Integer)
-    MENU_ID = db.Column(Integer)
+    MENU_ID = db.Column(Integer, primary_key=True)
+    STORE_ID = db.Column(Integer)
+    STAFF_ID = db.Column(Integer)   #登録・更新者を把握するのに必要
+    CLASS_1_ID = db.Column(Integer) #大分類を示すクラス
+    CLASS_1 = db.Column(String(5))
+    CLASS_2_ID = db.Column(Integer)    #中分類を示すクラス
+    CLASS_2 = db.Column(String(64))
+    CLASS_3_ID = db.Column(Integer)    #中分類を示すクラス
+    CLASS_3 = db.Column(String(64))    #小分類を示すクラス
 
     def __repr__(self):
-        return "(KIND='%s', NAME_OF_DISH='%s', PRICE='%s')" % (self.KIND, self.NAME_OF_DISH, self.PRICE)
-
+        return "(MENU_ID='%s', STORE_ID='%s', STAFF_ID='%s', CLASS_1_ID='%s', CLASS_1='%s', CLASS_2_ID='%s', CLASS_2='%s', CLASS_3_ID='%s', CLASS_3='%s')" % (self.MENU_ID, self.STORE_ID, self.STAFF_ID, self.CLASS_1_ID, self.CLASS_1, self.CLASS_2_ID, self.CLASS_2, self.CLASS_3_ID, self.CLASS_3)
 
 db.create_all()
 
-@app.route('/', methods = ['POST', 'GET'])
-def login():
-    #DBに会員情報を登録する
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/create_account', methods = ['POST', 'GET'])
+def create_account():
+    #Staffテーブルに会員情報を登録する
     if request.method == 'POST':
         e_mail = request.form['e_mail']
         password = hash_password(request.form['password'])
 
         try:
-            dupli_stores = db.session.query(Store).filter_by(E_MAIL=e_mail).one()
-            print(dupli_users.E_MAIL + " is exist")
-            return render_template('login.html')
+            double_create_check = db.session.query(Staff).filter_by(E_MAIL=e_mail).one()
+            print(double_create_check.E_MAIL + " is exist")
+            return render_template('regierror_1.html')
         except NoResultFound as ex:
             print(ex)
-            db.session.add(Store(E_MAIL=e_mail, PASSWORD=password))
-            db.session.commit()
-            db.session.close()
+            #ユーザー新規登録部分
+            try:
+                db.session.query(Staff).filter_by(STAFF_CLASS_ID=1, E_MAIL=e_mail).one()    #STAFF_CLASS_IDの代表者コードは1とする(今後一般職、マネージャーなどで利用できる権利を分けたい)
+                db.session.add(Staff(E_MAIL=e_mail, PASSWORD=password))
+                db.session.commit()
+                db.session.close()
+                return render_template('regierror_2.html')
+            except:
+                db.session.add(Staff(E_MAIL=e_mail, PASSWORD=password, STAFF_CLASS_ID=1, STAFF_CLASS="Representative")) #代表者として登録
+                store_create=db.session.query(Staff).filter_by(STAFF_CLASS_ID=1, E_MAIL=e_mail).one() #登録した代表者のレコードを抽出
+                store_id=store_create.STAFF_ID  #登録した代表者のIDを取得
+                print(store_id)
+                store_create.STORE_ID=store_id #登録した代表者のIDをSTORE_IDとしてstaffsテーブルに代入
+                db.session.add(Store(STORE_ID=store_id))    #代表者が登録された場合、新しいお店としてstoresテーブルに登録
+                db.session.commit()
+                db.session.close()
             return render_template('regicomp.html')
 
     return render_template('login.html')
+
+@app.route("/login", methods = ['POST', 'GET'])
+def login():
+    #DB情報を元にログイン
+    # session['logged_in'] = False
+
+    if request.method == 'POST':
+
+        e_mail = request.form['e_mail']
+        password = request.form['password']
+
+        try:
+            login_user = db.session.query(Staff).filter_by(E_MAIL=e_mail).one()
+            print(login_user)
+            session['store_id'] = login_user.STORE_ID
+            username_session = login_user.STORE_ID#デバック用
+            print(username_session)#デバック用
+            login_check = verify_password(login_user.PASSWORD, password)
+            # print(login_user)
+            if login_check == True:
+                #パスワードOKの処理
+                session['logged_in'] = True
+                return render_template('index.html')
+            else:
+                #パスワードNGの処理
+                return render_template('login_error.html')
+        except:
+            return render_template('login_error.html')
+
+    if session['logged_in']:
+        return render_template('index.html')
+    else:
+        return render_template('login.html')
 
 @app.route('/store_information_registration', methods = ['POST', 'GET'])
 def store_information_registration():
@@ -121,15 +182,11 @@ def store_information_registration():
             store_name = None
             return render_template('add_menu.html')
 
-    # return render_template('index.html')
-
-
-
 # メニューリスト表示
-@app.route('/add_menu' , methods = ['POST', 'GET'])
+@app.route('/show_menu' , methods = ['POST', 'GET'])
 def add_menu():
 
-        menu_infos = db.session.query(Food_Drink.ID, Food_Drink.KIND, Food_Drink.CLASS_MIDDLE, Food_Drink.NAME_OF_DISH, Food_Drink.PRICE).\
+        menu_list = db.session.query(Menu).filter_by(MENU_ID=menu_id, CLASS_1_ID=class_1_ID, CLASS_1=class_1, CLASS_2_ID=class_1_in, CLASS_2=class_2, CLASS_3_ID=class_3, CLASS_3=class_1).\
             all()
         class_middles = db.session.query(Food_Drink.CLASS_MIDDLE, Food_Drink.KIND).\
             distinct(Food_Drink.CLASS_MIDDLE).\
@@ -204,41 +261,6 @@ def create_menu():
 @app.route('/revise_menu')
 def revise_menu():
     return "未実装"
-
-
-@app.route("/index", methods = ['POST', 'GET'])
-def index():
-    #DB情報を元にログイン
-    # session['logged_in'] = False
-
-    if request.method == 'POST':
-
-        e_mail = request.form['e_mail']
-        password = request.form['password']
-
-        try:
-            login_user = db.session.query(Store).filter_by(E_MAIL=e_mail).one()
-            print(login_user)
-            session['store_id'] = login_user.STORE_ID
-            username_session = login_user.STORE_ID#デバック用
-            print(username_session)#デバック用
-            login_check = verify_password(login_user.PASSWORD, password)
-            # print(login_user)
-            if login_check == True:
-                #パスワードOKの処理
-                session['logged_in'] = True
-                return render_template('index.html')
-            else:
-                #パスワードNGの処理
-                return render_template('login_error.html')
-
-        except:
-            return render_template('login_error.html')
-
-    if session['logged_in']:
-        return render_template('index.html')
-    else:
-        return "ERROR"
 
 @app.route("/logout")
 def logout():
